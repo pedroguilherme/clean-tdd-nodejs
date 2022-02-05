fa279a16c4b2559f3ac7ac07c65ef4b0ecf374f9
Pedro Guilherme <pedroguilherme.scn@gmail.com>
2022-02-04 23:59:59 -0300

    feat: connect to mongodb before app listen

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,21 @@
 import supertest from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongodb'
 
 describe('SignUp Routes', function () {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? '')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
     await supertest(app)
       .post('/api/signup')
