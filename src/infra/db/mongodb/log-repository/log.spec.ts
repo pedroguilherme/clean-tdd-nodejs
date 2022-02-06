64e737ad9a70d1189a714a141504e83d0bd5a169
Pedro Guilherme <pedroguilherme.scn@gmail.com>
2022-02-06 18:15:47 -0300

    test: ensure LogMongoRepository create an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,27 @@
+import { MongoHelper } from '../helpers/mongodb'
+import { ServerError } from '../../../../presentation/errors'
+import { Collection } from 'mongodb'
+import { LogMongoRepository } from './log'
+
+describe('Log Mongo Repository', function () {
+  let errorCollection: Collection
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? '')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+
+  test('should create an error log on success', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError(new ServerError('any_stack'))
+    const errorSave = await errorCollection.countDocuments()
+    expect(errorSave).toBe(1)
+  })
+})
