8a639f688b7b735b386feb602c8638887c1365a2
Pedro Guilherme <pedroguilherme.scn@gmail.com>
2022-02-06 18:30:44 -0300

    refactor: ensure ExpressRouteAdapter return message error on error

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -6,9 +6,15 @@ export const expressRouteAdapter = (controller: Controller) => {
     const httpRequest: HttpRequest = {
       body: req.body
     }
-    void controller.handle(httpRequest)
-      .then(httpResponse => {
+    void controller.handle(httpRequest).then(httpResponse => {
+      if (httpResponse.statusCode === 500) {
+        res.status(httpResponse.statusCode)
+          .json({
+            error: httpResponse.body.message
+          })
+      } else {
         res.status(httpResponse.statusCode).json(httpResponse.body)
-      })
+      }
+    })
   }
 }
