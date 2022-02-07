e6a3e609373044dca955b0ef89d05bbeba7c3e20
Pedro Guilherme <pedroguilherme.scn@gmail.com>
2022-02-07 18:34:34 -0300

    refactor: remove glob and make import files from filesystem

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,11 +1,26 @@
 import { Express, Router } from 'express'
-import fg from 'fast-glob'
+import { readdirSync, lstatSync } from 'fs'
+import { join } from 'path'
+
+const isValidRouteFile = (file: string): boolean => {
+  return !file.endsWith('.map') && !file.endsWith('.test.ts')
+}
+
+const importValidRoutesFile = (pathDir: string, router: Router): void => {
+  readdirSync(pathDir).map(async file => {
+    if (isValidRouteFile(file)) {
+      (await import(join(pathDir, file))).default(router)
+    }
+  })
+}
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**/**routes.ts')
-    .map(
-      async file => (await import(`../../../${file}`)).default(router)
-    )
+  readdirSync(join(__dirname, '../routes')).map(async dir => {
+    const pathDir = join(__dirname, '../routes', dir)
+    if (lstatSync(pathDir).isDirectory()) {
+      importValidRoutesFile(pathDir, router)
+    }
+  })
 }
