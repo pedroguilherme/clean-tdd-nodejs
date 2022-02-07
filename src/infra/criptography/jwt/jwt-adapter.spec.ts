92a4e716b36e8ca07ab05770a7f7f4778a059140
Pedro Guilherme <pedroguilherme.scn@gmail.com>
2022-02-07 00:14:32 -0300

    refactor: move JwtAdapter to a factory method

--- src/infra/criptography/jwt/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt/jwt-adapter.spec.ts
@@ -7,20 +7,22 @@ jest.mock('jsonwebtoken', () => ({
   }
 }))
 
+const makeSut = (): JwtAdapter => new JwtAdapter('secret')
+
 describe('Jwt Adapter', function () {
   it('should call sign with correct values ', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
   })
   it('should return a token on sign success', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
   })
   it('should JwtAdapter throws if sign throws', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     jest.spyOn(jwt, 'sign').mockImplementationOnce(
       () => {
         throw new Error()
