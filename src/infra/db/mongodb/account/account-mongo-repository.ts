596295504d72c2fc1891eb585e2d55698acab780
Pedro Guilherme <pedroguilherme.scn@gmail.com>
2022-02-13 22:46:29 -0300

    feat: ensure AccountMongoRepository return an account on loadByEmail without role

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -5,8 +5,9 @@ import { MongoHelper } from '../helpers/mongodb'
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/account/load-account-by-email-repository'
 import { UpdateAccessTokenRepository } from '../../../../data/protocols/db/account/update-acces-token-repository'
 import { ObjectId } from 'mongodb'
+import { LoadAccountByTokenRepository } from '../../../../data/protocols/db/account/load-account-by-token-repository'
 
-export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository, LoadAccountByTokenRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
@@ -20,6 +21,15 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
   async loadByEmail (email: string): Promise<AccountModel | null> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const data = await accountCollection.findOne({ email })
+    if (!data) {
+      return null
+    }
+    return MongoHelper.map(data) as AccountModel
+  }
+
+  async loadByToken (accessToken: string, role?: string): Promise<AccountModel | null> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const data = await accountCollection.findOne({ accessToken, role })
     if (!data) {
       return null
     }
