da430217ebe227fce58acec45a183820a362b764
Pedro Guilherme <pedroguilherme.scn@gmail.com>
2022-02-21 18:39:50 -0300

    test: ensure SurveyMongoRepository load empty list

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -70,5 +70,13 @@ describe('Account Mongo Repository', function () {
     const sut = makeSut()
     const surveys = await sut.loadAll()
     expect(surveys.length).toBe(2)
+    expect(surveys[0].question).toBe('any_question')
+    expect(surveys[1].question).toBe('other_question')
+  })
+
+  test('Should load empty list', async () => {
+    const sut = makeSut()
+    const surveys = await sut.loadAll()
+    expect(surveys.length).toBe(0)
   })
 })
